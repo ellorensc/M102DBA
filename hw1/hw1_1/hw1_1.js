748f5d41328e91321a5afc52823e29d7d5f34ecf
eduardo.llorens <eduardo.llorensatrapalo.com>
2013-12-10 15:29:21 +0100

    M102 - Homework 1.1

---
+++ hw1/hw1_1/hw1_1.js
@@ -0,0 +1,12 @@
+/**
+ * Homework 1.1
+ *
+ * Download and install MongoDB from www.mongodb.org
+ * Then run the database as a single server instance on your PC (that is, run the mongod binary)
+ * Find the maxBsonObjectSize
+ *
+ * Solution: 16777216
+ */
+
+// Run at the shell the following command:
+db.isMaster().maxBsonObjectSize
